f4f3440f1c911b235f374b85addd394dd9e9df42
peter-cho <peter.cho@kakaocorp.com>
2018-12-01 16:13:04 +0900

    색상수정

--- gulpfile.js
+++ gulpfile.js
@@ -2,40 +2,40 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
+// var cp          = require('child_process');
 var cssnano 		= require('gulp-cssnano');
-
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+//
+// var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+// var messages = {
+//     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+// };
 
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
-
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
+// gulp.task('jekyll-build', function (done) {
+//     browserSync.notify(messages.jekyllBuild);
+//     return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+//         .on('close', done);
+// });
+//
+// /**
+//  * Rebuild Jekyll & do page reload
+//  */
+// gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+//     browserSync.reload();
+// });
+//
+// /**
+//  * Wait for jekyll-build, then launch the Server
+//  */
+// gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+//     browserSync({
+//         server: {
+//             baseDir: '_site'
+//         }
+//     });
+// });
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
@@ -57,13 +57,13 @@ gulp.task('sass', function () {
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-    gulp.watch(['assets/scss/*.scss', 'assets/scss/*/*.scss'], ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-});
+// gulp.task('watch', function () {
+//     gulp.watch(['assets/scss/*.scss', 'assets/scss/*/*.scss'], ['sass']);
+//     gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+// });
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+// gulp.task('default', ['browser-sync', 'watch']);
