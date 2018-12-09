31b0af422a1675054248fb5eab9bc8bb0c8b7230
peter.cho <peter.cho@kakaocorp.com>
2018-12-09 12:29:20 +0900

    내용 수정

--- demo/crc/design.js
+++ demo/crc/design.js
@@ -1,39 +1,57 @@
 var app = new Vue({
   el: '#design',
   methods: {
+    setData(collection, prop, value) {
+      collection[prop] = value;
+      localStorage.setItem('CRC', JSON.stringify(this.items));
+    },
     addItem() {
-      this.items = [this.item(), ...this.items]
+      this.setData(this, 'items', [this.item(), ...this.items])
+    },
+    addRespon(item) {
+      this.setData(item, 'respon', [...item.respon, this.respon()])
+    },
+    addCollabo(item) {
+      this.setData(item, 'collabo', [...item.collabo, this.collabo()])
     },
     item() {
       return {
         className: this.text('클래스명'),
-        respon: [this.text('책임')],
-        collabo: [this.text('연관 클래스')]
+        respon: [this.respon()],
+        collabo: [this.collabo()]
       };
+    },
+    respon() {
+      return this.text('책임');
+    },
+    collabo() {
+      return this.text('연관 클래스');
     },
     text(txt) {
       return {text: txt, modify: false}
     },
     toggleMode(prop) {
-      prop.modify = !prop.modify;
-    },
-    addRespon(item) {
-      item.respon.push(this.text('책임'));
-    },
-    addCollabo(item) {
-      item.collabo.push(this.text('연관 클래스'));
+      this.setData(prop, 'modify', !prop.modify);
     },
     removeItem(item){
-      this.items = this.items.filter(v => v != item);
+      this.setData(this, 'items', this.reject(this.items, item));
     },
     removeRespon(item, prop) {
-      item.respon = item.respon.filter(v => v != prop);
+      this.setData(item, 'respon', this.reject(item.respon, prop));
     },
     removeCollabo(item, prop) {
-      item.collabo = item.collabo.filter(v => v != prop);
+      this.setData(item, 'collabo', this.reject(item.collabo, prop));
+    },
+    reject(collection, value) {
+      return collection.filter(v => v != value);
     }
   },
-  data: {
-    items: []
+  data() {
+    return {
+      items: []
+    }
+  },
+  beforeMount() {
+    this.items = JSON.parse(localStorage.getItem('CRC'));
   }
 });
