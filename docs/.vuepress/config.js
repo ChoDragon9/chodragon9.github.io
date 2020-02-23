e787bd96ff2414cc28e7d79f61120505b68fa2d5
peter.cho <peter.cho@kakaocorp.com>
2020-02-23 12:23:06 +0900

    VuePress 추가

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  themeConfig: {
+    nav: [
+      {
+        text: 'Angular',
+        items: [
+          { text: 'RxJs 코드리뷰 사례', link: '/angular/rxjs-codereview/' },
+          { text: 'Angular의 강력함과 아쉬운점', link: '/angular/angular/' }
+        ]
+      }
+    ]
+  }
+}
