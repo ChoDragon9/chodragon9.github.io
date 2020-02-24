261a036011c1777087089b606355d41b1fff9ab9
peter.cho <peter.cho@kakaocorp.com>
2020-02-24 09:08:21 +0900

    카테고리 수정

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,78 +3,6 @@ module.exports = {
   themeConfig: {
     nav: [
       {
-        text: '패러다임',
-        items: [
-          {
-            text: '객체지향',
-            items: [
-              { text: '객체 정리', link: '/blog/object/' },
-              { text: 'produce 구현해보기 - OOP 버전', link: '/blog/produce-oop/' }
-            ]
-          },
-          {
-            text: '함수형',
-            items: [
-              { text: '함수형 프로그래밍 정리', link: '/blog/functional/' },
-              { text: '함수형 프로그래밍 정리2', link: '/blog/functional2/' },
-              { text: 'produce 구현해보기', link: '/blog/produce/' },
-              { text: 'produce 구현해보기 - 불변성편', link: '/blog/produce-immutable/' },
-            ]
-          },
-        ]
-      },
-      {
-        text: '언어',
-        items: [
-          {
-            text: 'Javascript',
-            items: [
-              {text: '[ECMAScript] ES6 Overview', link: '/blog/es6/'},
-              {text: 'this, this is magic.', link: '/blog/this/'},
-              {text: '콜백 사용 시점', link: '/blog/callback/'},
-              {text: '안전한 체이닝을 위한 safeChain', link: '/blog/safe-chain/'},
-              {text: 'async await 정리', link: '/blog/async-await-case/'},
-              {text: '옵져버 매커니즘', link: '/blog/observation-mechanism/'},
-              {text: '코루틴을 사용한 지연 평가', link: '/blog/call-stack-and-coroutine/'},
-              {text: '[손에 잡히는 정규표현식] 역참조/전방탐색/후방탐색', link: '/blog/regexp-book-backref/'},
-              {text: 'Promise 정리', link: '/blog/promise/'},
-            ]
-          },
-          {
-            text: 'SML',
-            items: [
-              {text: '기초', link: '/blog/sml-basic/'},
-              {text: '빌트인 자료형', link: '/blog/sml-builtin-datatype/'},
-              {text: '자료형 정의', link: '/blog/sml-datatype/'},
-              {text: '예외처리', link: '/blog/sml-exception/'},
-              {text: '함수', link: '/blog/sml-function/'},
-              {text: '고차함수', link: '/blog/sml-higher-order-function/'},
-              {text: 'infix 정의', link: '/blog/sml-infix/'},
-              {text: '모듈 시스템', link: '/blog/sml-module/'},
-            ]
-          },
-        ]
-      },
-      {
-        text: '오픈소스',
-        items: [
-          {
-            text: 'Angular',
-            items: [
-              {text: 'RxJs 코드리뷰 사례', link: '/blog/rxjs-codereview/'},
-              {text: 'Angular의 강력함과 아쉬운점', link: '/blog/angular/'}
-            ]
-          },
-          {
-            text: 'Vue',
-            items: [
-              {text: 'Nuxt에서 TypeScript로 개발하기', link: '/blog/nuxt-ts/'},
-              {text: 'Nuxt v2.0에서 v2.11로 마이그레이션', link: '/blog/nuxt-migration/'},
-            ]
-          },
-        ]
-      },
-      {
         text: '설계',
         items: [
           {
@@ -110,36 +38,123 @@ module.exports = {
         ]
       },
       {
+        text: '패러다임',
+        items: [
+          {
+            text: '객체지향',
+            items: [
+              {text: '객체 정리', link: '/blog/object/'},
+              {text: 'produce 구현해보기 - OOP 버전', link: '/blog/produce-oop/'}
+            ]
+          },
+          {
+            text: '함수형',
+            items: [
+              {text: '함수형 프로그래밍 정리', link: '/blog/functional/'},
+              {text: '함수형 프로그래밍 정리2', link: '/blog/functional2/'},
+              {text: 'produce 구현해보기', link: '/blog/produce/'},
+              {text: 'produce 구현해보기 - 불변성편', link: '/blog/produce-immutable/'},
+            ]
+          },
+        ]
+      },
+      {
+        text: '언어',
+        items: [
+          {
+            text: 'TypeScript',
+            items: [
+              {text: '인하우스 서비스 구축하며 사용했던 TypeScript 문법 정리', link: '/blog/typescript-from-inhouse-service/'},
+            ]
+          },
+          {
+            text: 'Javascript',
+            items: [
+              {text: '[ECMAScript] ES6 Overview', link: '/blog/es6/'},
+              {text: 'this, this is magic.', link: '/blog/this/'},
+              {text: 'Promise 정리', link: '/blog/promise/'},
+              {text: 'async await 정리', link: '/blog/async-await-case/'},
+              {text: '코루틴을 사용한 지연 평가', link: '/blog/call-stack-and-coroutine/'},
+            ]
+          },
+          {
+            text: 'SML',
+            items: [
+              {text: '기초', link: '/blog/sml-basic/'},
+              {text: '빌트인 자료형', link: '/blog/sml-builtin-datatype/'},
+              {text: '자료형 정의', link: '/blog/sml-datatype/'},
+              {text: '예외처리', link: '/blog/sml-exception/'},
+              {text: '함수', link: '/blog/sml-function/'},
+              {text: '고차함수', link: '/blog/sml-higher-order-function/'},
+              {text: 'infix 정의', link: '/blog/sml-infix/'},
+              {text: '모듈 시스템', link: '/blog/sml-module/'},
+            ]
+          },
+        ]
+      },
+      {
+        text: '오픈소스',
+        items: [
+          {
+            text: 'Angular',
+            items: [
+              {text: 'RxJs 코드리뷰 사례', link: '/blog/rxjs-codereview/'},
+              {text: 'Angular의 강력함과 아쉬운점', link: '/blog/angular/'}
+            ]
+          },
+          {
+            text: 'Vue',
+            items: [
+              {text: 'Nuxt에서 TypeScript로 개발하기', link: '/blog/nuxt-ts/'},
+              {text: 'Nuxt v2.0에서 v2.11로 마이그레이션', link: '/blog/nuxt-migration/'},
+            ]
+          },
+          {
+            text: 'React',
+            items: [
+              {text: '코드리뷰를 위한 React 컨벤션', link: '/blog/react-convention-for-codereview/'},
+            ]
+          },
+        ]
+      },
+      {
         text: '정리',
         items: [
           {
             text: '관리',
             items: [
-              { text: '개발 일정산정', link: '/blog/estimation/' },
-              { text: '배포 어디까지 알고 있나요?', link: '/blog/release/' },
-              { text: '효율적인 회의를 위한 퍼실리테이션', link: '/blog/facilitation-for-meeting/' },
-              { text: '위험상황을 대비하는 위험관리', link: '/blog/preventive-maintenance/' },
-              { text: '형상관리 전략정리', link: '/blog/git-scm-experience/' },
+              {text: '개발 일정산정', link: '/blog/estimation/'},
+              {text: '배포 어디까지 알고 있나요?', link: '/blog/release/'},
+              {text: '효율적인 회의를 위한 퍼실리테이션', link: '/blog/facilitation-for-meeting/'},
+              {text: '위험상황을 대비하는 위험관리', link: '/blog/preventive-maintenance/'},
+              {text: '형상관리 전략정리', link: '/blog/git-scm-experience/'},
             ]
           },
           {
-            text: '정리자료',
+            text: '도서',
             items: [
-              { text: '프로그래밍의 정석 도서 정리', link: '/blog/programming-standards/' },
-              { text: '소프트웨어의 핵심 비기능 요구사항', link: '/blog/non-functional-requirement/' },
-              { text: '에러 처리 정리', link: '/blog/how-to-catch-error/' },
-              { text: 'CORS와 HTTP Cookie', link: '/blog/cors-and-http-cookie/' },
-              { text: '추상화 정리', link: '/blog/abstract/' },
-              { text: '코드리뷰를 위한 React 컨벤션', link: '/blog/react-convention-for-codereview/' },
-              { text: '경우에 따른 clone 함수', link: '/blog/clone-function/' },
-              { text: '분기를 작성하는 방법 n가지', link: '/blog/branch-n-case/' },
-              { text: '인하우스 서비스 구축하며 사용했던 TypeScript 문법 정리', link: '/blog/typescript-from-inhouse-service/' },
+              {text: '프로그래밍의 정석 도서 정리', link: '/blog/programming-standards/'},
+              {text: '소프트웨어의 핵심 비기능 요구사항', link: '/blog/non-functional-requirement/'},
+              {text: '[손에 잡히는 정규표현식] 역참조/전방탐색/후방탐색', link: '/blog/regexp-book-backref/'},
+            ]
+          },
+          {
+            text: '경험',
+            items: [
+              {text: 'CORS와 HTTP Cookie', link: '/blog/cors-and-http-cookie/'},
+              {text: '에러 처리 정리', link: '/blog/how-to-catch-error/'},
+              {text: '추상화 정리', link: '/blog/abstract/'},
+              {text: '분기를 작성하는 방법 n가지', link: '/blog/branch-n-case/'},
+              {text: '콜백 사용 시점', link: '/blog/callback/'},
+              {text: '경우에 따른 clone 함수', link: '/blog/clone-function/'},
+              {text: '안전한 체이닝을 위한 safeChain', link: '/blog/safe-chain/'},
+              {text: '옵져버 매커니즘', link: '/blog/observation-mechanism/'},
             ]
           },
         ]
       },
-      { text: 'About', link: '/about' },
-      { text: 'Github', link: 'https://github.com/ChoDragon9', target: '_blank'}
+      {text: 'About', link: '/about'},
+      {text: 'Github', link: 'https://github.com/ChoDragon9', target: '_blank'}
     ]
   },
   plugins: [
